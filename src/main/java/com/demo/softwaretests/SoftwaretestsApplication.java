57672a393e03d261f3f129d2cd678819a99c26fa
Pablo Pomodoro <p.pomodoro@posteo.com>
2025-02-13 16:59:20 +0100

    feat: initialize spring boot project

---
+++ src/main/java/com/demo/softwaretests/SoftwaretestsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.softwaretests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwaretestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwaretestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/softwaretests/SoftwaretestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.softwaretests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwaretestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
