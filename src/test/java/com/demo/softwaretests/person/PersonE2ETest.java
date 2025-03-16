0ff08d8816ef8ffd0a90a48eea180684e402f474
Pablo Pomodoro <p.pomodoro@posteo.com>
2025-03-16 02:50:07 +0100

    test: e2e test adaptions

--- src/test/java/com/demo/softwaretests/person/PersonE2ETest.java
+++ src/test/java/com/demo/softwaretests/person/PersonE2ETest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import java.time.LocalDate;
 
@@ -16,20 +17,18 @@ class PersonE2ETest {
 
     @BeforeEach
     void setUp() {
-        // Initialize TestRestTemplate with the default configuration
         restTemplate = new TestRestTemplate();
-        // Set the base URL to the running instance
-        restTemplate.getRestTemplate().setUriTemplateHandler(
-                new org.springframework.web.util.DefaultUriBuilderFactory("http://localhost:42069")
-        );
+        restTemplate.getRestTemplate().setUriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:1337"));
     }
 
     @Test
     void givenPersonWithEmailDomain_whenGetAllPersonsByEmailDomain_thenReturnRichard() {
-        String domain = "gmail.com";
+        var domain = "gmail.com";
 
         ResponseEntity<String> response = restTemplate.getForEntity(
-                "/persons/email-domain/{domain}", String.class, domain);
+                "/persons/email-domain/{domain}",
+                String.class,
+                domain);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody())
@@ -44,7 +43,8 @@ class PersonE2ETest {
 
         ResponseEntity<String> response = restTemplate.getForEntity(
                 "/persons/age-range?fromAge={fromAge}&toAge={toAge}",
-                String.class, fromAge, toAge);
+                String.class,
+                fromAge, toAge);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody())
@@ -54,28 +54,32 @@ class PersonE2ETest {
 
     @Test
     void givenPersonDetails_whenCreatePerson_thenReturnCreated() {
-        String firstName = "Emma";
-        String lastName = "Eidhoven";
-        String email = "emma.eidhoven@hotmail.com";
-        LocalDate dateOfBirth = LocalDate.of(1990, 1, 1);
+        var firstName = "Emma";
+        var lastName = "Eidhoven";
+        var email = "emma.eidhoven@hotmail.com";
+        var dateOfBirth = LocalDate.of(1990, 1, 1);
 
         ResponseEntity<String> response = restTemplate.postForEntity(
                 "/persons/create?firstName={firstName}&lastName={lastName}&email={email}&dateOfBirth={dateOfBirth}",
-                null, String.class, firstName, lastName, email, dateOfBirth.toString());
+                null,
+                String.class,
+                firstName, lastName, email, dateOfBirth.toString());
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
 
     @Test
     void givenInvalidAge_whenCreatePerson_thenReturnBadRequest() {
-        String firstName = "Junior";
-        String lastName = "Jamestown";
-        String email = "junior.jamestown@hotmail.com";
-        LocalDate dateOfBirth = LocalDate.of(2010, 1, 1);
+        var firstName = "Junior";
+        var lastName = "Jamestown";
+        var email = "junior.jamestown@hotmail.com";
+        var dateOfBirth = LocalDate.of(2010, 1, 1);
 
         ResponseEntity<String> response = restTemplate.postForEntity(
                 "/persons/create?firstName={firstName}&lastName={lastName}&email={email}&dateOfBirth={dateOfBirth}",
-                null, String.class, firstName, lastName, email, dateOfBirth.toString());
+                null,
+                String.class,
+                firstName, lastName, email, dateOfBirth.toString());
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         assertThat(response.getBody())
@@ -85,32 +89,20 @@ class PersonE2ETest {
 
     @Test
     void givenEmailAlreadyInUse_whenCreatePerson_thenReturnBadRequest() {
-        String firstName = "Fred";
-        String lastName = "Faker";
-        String email = "richard.ruediger@gmail.com";
-        LocalDate dateOfBirth = LocalDate.of(1990, 1, 1);
+        var firstName = "Fred";
+        var lastName = "Faker";
+        var email = "richard.ruediger@gmail.com";
+        var dateOfBirth = LocalDate.of(1990, 1, 1);
 
         ResponseEntity<String> response = restTemplate.postForEntity(
                 "/persons/create?firstName={firstName}&lastName={lastName}&email={email}&dateOfBirth={dateOfBirth}",
-                null, String.class, firstName, lastName, email, dateOfBirth.toString());
+                null,
+                String.class,
+                firstName, lastName, email, dateOfBirth.toString());
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         assertThat(response.getBody())
                 .contains("\"errorMessage\":\"Person could not be created!\"")
                 .contains("\"exceptionReason\":\"The email address: " + email + " is already in use.\"");
-    }
-
-    @Test
-    void givenValidParameters_whenCreatePerson_thenReturnCreated() {
-        String firstName = "Emma";
-        String lastName = "Eidhoven";
-        String email = "emma.eidhoven@hotmail.com";
-        LocalDate dateOfBirth = LocalDate.of(1990, 1, 1);
-
-        ResponseEntity<String> response = restTemplate.postForEntity(
-                "/persons/create?firstName={firstName}&lastName={lastName}&email={email}&dateOfBirth={dateOfBirth}",
-                null, String.class, firstName, lastName, email, dateOfBirth.toString());
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
 }
