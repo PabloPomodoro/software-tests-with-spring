822dec9bf31642fd1db677d56e1ecfa9d0f0bc1c
Pablo Pomodoro <p.pomodoro@posteo.com>
2025-03-16 02:47:42 +0100

    feat: test data config + data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (id, full_name, email_address, age) VALUES
+    (1, 'Richard Rüdiger', 'richard.ruediger@gmail.com', 41),
+    (2, 'Günther Grandiger', 'guenther.grandiger@gmail.com', 55),
+    (3, 'Lilliane Langdorf', 'lilliane.langdorf@icloud.com', 29),
+    (4, 'Bianca Bambus', 'bianca.bambus@yahoo.com', 22);
